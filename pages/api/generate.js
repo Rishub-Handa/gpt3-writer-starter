bfa8d22adc5f4650c4df400c7a7057ee6e07f670
Rishub Handa <rishubhanda@Rishubs-MacBook-Pro.local>
2022-12-30 15:16:31 -0500

    wrapped up project

---
+++ pages/api/generate.js
@@ -0,0 +1,27 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = "Create a story with the following parts: \n";
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}\n`)
+
+  const baseCompletion = await openai.createCompletion({
+    // Does the curie model work ok? 
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 650,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
